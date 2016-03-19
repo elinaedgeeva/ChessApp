1cf30f93981ddf7aa61fbe9004c29fa9cacd821f
gottliebe <jools.semchenkova@gmail.com>
2016-03-19 12:10:09 +0300

    test1

---
+++ TeamProjectChess/Model/PlayerCard.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace TeamProjectChess.Model
+{
+   public class PlayerCard
+    {
+
+        public int ID { get; set; }
+        public int Rank { get; set; }
+        public string ImageName { get; set; }
+        public string Name { get; set; }
+        public Image Image { get; set; }
+        public int BirthYear { get; set; }
+
+        public string  Country { get; set; }
+
+        public int ClassicRateValue { get; set; }
+        public int BlitzRateValue { get; set; }
+        public int RapidRateValue { get; set; }
+        public PlayerCard(int r, string n, string imn)
+        {
+            ImageName = imn;
+            Rank = r;
+            Name = n;
+        }
+
+    }
+}
